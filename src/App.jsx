15ea03bc6dd0b3bcd8c4a257d604b9201841b658
Joe Watkins <joewatkinspdx@gmail.com>
2023-02-11 14:28:55 -0800

    finish img tute

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.scss'
 
 function App() {
   const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
   const configuration = new Configuration({
     apiKey: import.meta.env.VITE_Open_AI_Key,
   });
@@ -16,7 +17,7 @@ function App() {
       n: 1,
       size: "1024x1024",
     });
-    console.log(res.data.data[0].url);
+    setResult(res.data.data[0].url);
   } 
 
   return (
@@ -28,6 +29,7 @@ function App() {
         onChange={(e) => setPrompt(e.target.value)} 
       />
       <button onClick={generateImage}>Generate an Image</button>
+      {result.length > 0 ? <img className="result-image" src={result} alt={prompt} /> : <></>}
     </div>
   );
 }
